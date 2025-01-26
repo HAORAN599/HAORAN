d29bd30d1e4ef096ca5ee3b0b007a96597b49759
HAORAN WEI <weihaoran2024@gmail.com>
2025-01-26 17:34:06 +0000

    add a function that can input the number to calculate the greatest common number

--- The Euclidean Algorithm.py
+++ The Euclidean Algorithm.py
@@ -1,7 +1,20 @@
+A = int(input("please enter the first number "))
+B = int(input("please enter the second number "))
+
 def GCD(A,B):  #use def to encapsulated code
-    C = A % B  #define C,to caculate remainder
+    
     if B == 0:  
-       return A  #if B == 0,means the greast common divisor is A
+       return A  #if B == 0,means the greast common divisor is A   
     else:
-       GCD(B, C)  #if not,continue to caculate, when B == 0,program stop
+       return GCD(B, A % B)  #if not,continue to caculate, when B == 0,program stop
+
+while  A < 0 or B < 0:
+    print("the number must greater than or equal to 0,please try again")
+    A = int(input("please enter the first number "))
+    B = int(input("please enter the second number "))
+    
+print("the Greatest Common Divisor between",A,"and",B,"is",GCD(A, B))
+
+
+
        
